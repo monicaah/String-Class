4ce6884326de5b402c546df8ab0772bfb9313b6d
andela-mkwamboka <monicah.kwamboka@andela.com>
2016-07-27 16:20:01 +0300

    Add test assertions

--- spec/stringspec.js
+++ spec/stringspec.js
@@ -5,24 +5,30 @@ describe('hasVowels', () => {
 
   it('Returns false if the string doesn\'t contain vowels', () => {
     expect('cry'.hasVowels()).toBeFalse();
+    expect('@#!324'.hasVowels()).toBeFalse()
   });
 });
 
 describe('toUpper', () => {
   it('Returns the String with all it\'s characters in upper case', () => {
     expect('country'.toUpper()).toEqual('COUNTRY');
+    expect('Country'.toUpper()).toEqual('COUNTRY');
+    expect('cou153ntry'.toUpper()).toEqual('COU153NTRY');
   });
 });
 
 describe('toLower', () => {
   it('Returns the String with all it\'s characters in lower case', () => {
     expect('MACHO'.toLower()).toEqual('macho');
+    expect('mACHO'.toLower()).toEqual('macho');
+    expect('M34achO'.toLower()).toEqual('m34acho');
   });
 });
 
 describe('ucFirst', () => {
   it('Returns the String with the First Character Upper case', () => {
     expect('google'.ucFirst()).toEqual('Google');
+    expect('123google'.ucFirst()).toEqual('123Google');
   });
 });
 
@@ -40,12 +46,16 @@ describe('isQuestion', () => {
 describe('words', () => {
   it('Returns a list of the words in the string, as an Array', () => {
     expect('Oh my word'.words()).toEqual(['Oh', 'my', 'word']);
+    expect('Oh my @#@$ word'.words()).toEqual(['Oh', 'my', 'word']); 
+    expect('!!! @@@ ???'.words()).toEqual([]);
   });
 });
 
 describe('wordCount', () => {
   it('Returns the number of words in the string', () => {
     expect('Incomprehensibilities'.wordCount()).toEqual(21);
+    expect('Glass123'.wordCount()).toEqual(8);
+    expect('1234 Newsletter'.wordCount()).toEqual(15);
   });
 });
 
